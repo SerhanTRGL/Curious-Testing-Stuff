065286b7341c8df8c330ef319d5c441c1e65ed17
Serhan Turgul <serhanturgul2@gmail.com>
2021-04-07 17:35:57 +0300

    Messed up everythin

--- the set of al possible strings from alphabet.c
+++ the set of al possible strings from alphabet.c
@@ -1,27 +1,79 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define TRUE 1
+#define FALSE 0
 
-int main(void){
-    char    alphabet[2] = {'a', 'b'};
-    char    current_char;
-    int const     length_of_alphabet = 2;
-    int     currently_how_many_chars_on_screen = 1;
+/* This function sets all the characters of
+** a string to the first character of the alphabet
+*/
+void resetString(char *string, char *alphabet, int size){
+    int i;
+    for(i = 0; i<size; i++){
+        string[i] = alphabet[0];
+    }
+}
 
-    while(1){
-        int     i, j;
-
-        for(i = 0; i<currently_how_many_chars_on_screen; i++){
-            for(j = 0; j<length_of_alphabet; j++){
-                printf("%c", alphabet[j]);
-                //selamın aleyküm
-            }
+/* This function checks if all the characters of a string
+** is the last characters of an alphabet. This is used to
+** if the last string in a given length is printed out,
+** then increase the length of the string by one.
+*/
+int checkIfAllLastChar(char *string, char *alphabet, int size){
+    int i;
+    for(i = 0; i<size; i++){
+        if(string[i]!=alphabet[strlen(alphabet)-1]){
+            return FALSE;
         }
-        printf("\n");
-        currently_how_many_chars_on_screen++;
-    printf("%d", currently_how_many_chars_on_screen);
-    sleep(1);
+    }
+    return TRUE;
+}
+
+#if 0 //This was for testing
+void makeAllLastChar(char *string, char *alphabet, int size){
+    int i;
+    for(i = 0; i<size; i++){
+        string[i] = alphabet[strlen(alphabet)-1];
+    }
+}
+#endif // 0
+
+/* The function below doesn't work properly
+** and I don't exactly know why.
+*/
+void PrintAllStrings(char *string, char *alphabet, int size){
+    int i;
+    int j;
+    if(checkIfAllLastChar(string, alphabet, size)==TRUE){
+        if(size == 0){
+            printf("\n");
+            usleep(300000);
+            return;
+        }
+        return;
     }
 
-    return EXIT_SUCCESS;
+    for(i = 0; i<size; i++){
+        for(j = 0; j<strlen(alphabet); j++){
+            string[i] = alphabet[j];
+            printf("%c", string[0]);
+            PrintAllStrings(string+1, alphabet, size-1);
+        }
+    }
 }
+
+int main(void){
+    char alphabet[2] = {'a', 'b'};
+    char *string;
+    int size = 1;
+
+    string = (char *)malloc(sizeof(char));
+
+    for(;;){
+        resetString(string, alphabet, size);
+        PrintAllStrings(string, alphabet, size);
+        size++;
+        string = realloc(string, size);
+    }
+}
+
